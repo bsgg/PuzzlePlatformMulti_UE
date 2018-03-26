1d75026ad2a917682c95a1f691b8b39e0648b08d
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-03-26 08:53:26 +0100

    Include transition map

--- PuzzlePlatformMulti/Source/PuzzlePlatformMulti/LobbyGameMode.cpp
+++ PuzzlePlatformMulti/Source/PuzzlePlatformMulti/LobbyGameMode.cpp
@@ -6,15 +6,25 @@
 
 void ALobbyGameMode::PostLogin(APlayerController* newPlayer)
 {
+	Super::PostLogin(newPlayer);
+
 	++NumberOfPlayers;
 
 	if (NumberOfPlayers >= 3)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Reached 3 Players!"));
+
+		UWorld* world = GetWorld();
+
+		if (!ensure(world != nullptr)) return;
+		bUseSeamlessTravel = true;
+		world->ServerTravel("/Game/PuzzlePlatforms/Maps/Game?listen");
 	}
 }
 
 void ALobbyGameMode::Logout(AController* Existing)
 {
+	Super::Logout(Existing);
+
 	--NumberOfPlayers;
 }
