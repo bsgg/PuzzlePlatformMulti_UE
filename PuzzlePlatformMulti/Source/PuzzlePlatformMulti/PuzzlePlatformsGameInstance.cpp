24c69526516a5109bdeaa5d38de35ae2a798b39c
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-03-27 08:49:54 +0100

    Fixed bug with number of players in the current session

--- PuzzlePlatformMulti/Source/PuzzlePlatformMulti/PuzzlePlatformsGameInstance.cpp
+++ PuzzlePlatformMulti/Source/PuzzlePlatformMulti/PuzzlePlatformsGameInstance.cpp
@@ -13,7 +13,7 @@
 #include "PlatformTrigger.h"
 
 
-const static FName SESSION_NAME = TEXT("My Session Game");
+const static FName SESSION_NAME = TEXT("Game");
 const static FName SERVER_NAME_SETTINGS_KEY = TEXT("ServerName");
 
 UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectInitializer)
